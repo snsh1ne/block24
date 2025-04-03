6be12262da00dd7c57b919384e2313c42facb31f
Sara Yared <sarayared@gmail.com>
2025-04-03 19:36:11 -0400

    inital push

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
