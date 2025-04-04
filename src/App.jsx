76d63759aca77b4862fd7b3721aeb282b3c4b351
Sara Yared <sarayared@gmail.com>
2025-04-03 21:12:19 -0400

    everything but css

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,41 @@
-import './App.css'
+import "./App.css";
+import { puppyList } from "./data.js";
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
+  console.log(featuredPup);
 
   return (
-    <>
-      <div>
-        
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+    <div className="App">
+      {puppies.map((puppy) => {
+        return (
+          <p
+            onClick={() => {
+              setFeatPupId(puppy.id);
+            }}
+            key={puppy.id}
+          >
+            {puppy.name}
+          </p>
+        );
+      })}
+
+      <p>
+        {featPupId && (
+          <div>
+            <h2>{featuredPup.name}</h2>
+            <ul>
+              <li>Age: {featuredPup.age}</li>
+              <li>Email: {featuredPup.email}</li>
+            </ul>
+          </div>
+        )}
       </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
